c9fd4e32fb4086b638cc7ffd80f54e7b654abdc4
huanmingchang <huanming1988@gmail.com>
2022-08-28 21:34:42 +0800

    feat: 設定 react hook form 表單驗證功能
    因應新需求做調整：
      驗整表單功能引進 react hook form 來做
      設定相關規則來做驗證並加入錯誤訊息
    
    調整項目：
    1. 設定 react hook form 驗證規則以及錯誤訊息
    2. 設定 isProcessing 來控制之後登入 disable 狀態

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,19 @@
 import navlogo from '../images/navlogo.png'
 import homepage from '../images/homepage-bg.png'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 
 const Register = () => {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm()
+  const onSubmit = (data) => console.log(data)
+
   return (
     <div id='signUpPage' className='bg-yellow'>
       <div className='conatiner signUpPage vhContainer'>
@@ -13,7 +24,7 @@ const Register = () => {
           <img className='d-m-n' src={homepage} alt='workImg' />
         </div>
         <div>
-          <form className='formControls'>
+          <form className='formControls' onSubmit={handleSubmit(onSubmit)}>
             <h2 className='formControls_txt'>註冊帳號</h2>
             <label className='formControls_label' htmlFor='email'>
               Email
@@ -24,8 +35,16 @@ const Register = () => {
               id='email'
               name='email'
               placeholder='請輸入 email'
-              required
+              {...register('email', {
+                required: { value: true, message: '此欄位不可留空' },
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                  message: '不符合 Email 規則',
+                },
+              })}
             />
+            <span>{errors.email?.message}</span>
+
             <label className='formControls_label' htmlFor='name'>
               您的暱稱
             </label>
@@ -35,7 +54,12 @@ const Register = () => {
               name='name'
               id='name'
               placeholder='請輸入您的暱稱'
+              {...register('name', {
+                required: { value: true, message: '此欄位不可留空' },
+              })}
             />
+            <span>{errors.name?.message}</span>
+
             <label className='formControls_label' htmlFor='pwd'>
               密碼
             </label>
@@ -45,22 +69,36 @@ const Register = () => {
               name='pwd'
               id='pwd'
               placeholder='請輸入密碼'
-              required
+              {...register('pwd', {
+                required: { value: true, message: '此欄位不可留空' },
+                minLength: { value: 6, message: 'password 需大於 6 碼' },
+              })}
             />
+            <span>{errors.pwd?.message}</span>
+
             <label className='formControls_label' htmlFor='pwd'>
               再次輸入密碼
             </label>
             <input
               className='formControls_input'
               type='password'
-              name='pwd'
-              id='pwd'
+              name='confirmPwd'
+              id='confirmPwd'
               placeholder='請再次輸入密碼'
-              required
+              {...register('confirmPwd', {
+                required: { value: true, message: '此欄位不可留空' },
+                validate: (value: string) => {
+                  if (watch('pwd') != value) {
+                    return '密碼不一致'
+                  }
+                },
+              })}
             />
+            <span>{errors.confirmPwd?.message}</span>
+
             <input
               className='formControls_btnSubmit'
-              type='button'
+              type='submit'
               value='註冊帳號'
             />
             <Link className='formControls_btnLink' to='/login'>
