d4e76b98a71316975878378b08c163dbd895b1de
huanmingchang <huanming1988@gmail.com>
2022-08-31 20:09:40 +0800

    feat: 新增 useContext 以及 useContext 來處理 token 邏輯
    因應新需求做調整：
     之前的邏輯只有把 token 放在 Login 元件裡面，其他元件無法使用
     故改用 createContext / useContext 來改寫，讓 token 可以在多個元件之中使用
    
    調整項目：
    1. Context.js，新增 createContext 和 useContext 邏輯並 export 給其他元件使用
    2. App.js，將 token / setToken 拉到這層，並用 provider 輸出給其他元件使用
    3. Login.js，使用 useContext 來使用 App.js 傳入到 token 及 setToken，登入之後 setToken 改變 token 的值

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import navlogo from '../images/navlogo.png'
 import homepage from '../images/homepage-bg.png'
+import { useAuth } from './Context'
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { useState } from 'react'
@@ -8,7 +9,7 @@ import Swal from 'sweetalert2'
 
 const Login = () => {
   const [isProcessing, setIsProcessing] = useState(false)
-  const [token, setToken] = useState('')
+  const { token, setToken } = useAuth()
   const navigate = useNavigate()
   const {
     register,
